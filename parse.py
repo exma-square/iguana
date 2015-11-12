66f88d0c286480bc25a1f2cb4c29b88060e89a47
Billyking <billyking19920205@gmail.com>
2015-11-13 00:33:33 +0800

    Get ettoday hotnews title and url

--- parse.py
+++ parse.py
@@ -1,4 +1,33 @@
+
+# coding: utf-8
+
+# In[23]:
+
 import requests
 from bs4 import BeautifulSoup
-res = requests.get("http://www.ettoday.net")
-print res.text
+hotnewsReq = requests.get("http://www.ettoday.net/news/hot-news.htm")
+
+
+# In[22]:
+
+hotnewsSoup = BeautifulSoup(hotnewsReq.text)
+
+
+# In[43]:
+
+hotnewsTitle = []
+for titles in hotnewsSoup.select('h3 a'):
+    hotnewsTitle.append([titles.text , titles.get('href')])
+
+
+# In[53]:
+
+for lists in hotnewsTitle:
+    [title,href] = lists
+    print title , href
+
+
+# In[ ]:
+
+
+
