42c95790ce6e2d1ddb814e1c80e5dd79b7b8c84e
v123582 <v123582@gmail.com>
2015-11-14 00:33:47 +0800

    using mongo to store data

--- parse.py
+++ parse.py
@@ -1,5 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+from datetime import datetime
+
+# Global Varibles
+collection = MongoClient('localhost', 27017).iguana.ettoday
+
+
 hotnewsReq = requests.get("http://www.ettoday.net/news/hot-news.htm")
 hotnewsSoup = BeautifulSoup(hotnewsReq.text)
 
@@ -14,11 +21,23 @@ for titles in hotnewsSoup.select('.part_pictxt_2 h3 a'):
     img = detailSoup.select('meta[property="og:image"]')[0].get('content')
     keywords = [keyword.text for keyword in detailSoup.select('.menu_txt_2 a strong')]
     hotnews.append([titles.text , url , img , keywords])
+
 for lists in hotnews:
-    [title,href,img , keywords] = lists
-    print title , href , img
-    for keyword in keywords:
-        print keyword
-
-
-
+    [title, url, img, keywords] = lists
+    # print title, href ,img, keywords
+    dataObject = {
+        "source": 'ettoday',
+        "newsTitle" : title,
+        "url" : url,
+        "image" : img,
+        "keywords" : keywords,
+        "content" : "",
+        "tag" : [],
+        "title" : "",
+        "name" : "",
+        "comment" : [],
+        "dislike" : [],
+        "like" : [],
+        "create_date" : ''
+    }
+    collection.insert_one(dataObject)
