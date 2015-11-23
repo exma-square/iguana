9a227497702cdbfc36f89b5fd18ec3a627a0a451
Billyking <billyking19920205@gmail.com>
2015-11-23 17:03:23 +0800

    finish update info and news info separate & solve duplicate news insert issue

--- parse.py
+++ parse.py
@@ -4,9 +4,6 @@ from bs4 import BeautifulSoup
 from pymongo import MongoClient
 from datetime import datetime
 
-# Global Varibles
-collection = MongoClient('localhost', 27017).iguana.ettoday
-
 
 hotnewsReq = requests.get("http://www.ettoday.net/news/hot-news.htm")
 hotnewsSoup = BeautifulSoup(hotnewsReq.text, "html.parser")
@@ -18,7 +15,7 @@ for titles , content, time in zip(hotnewsSoup.select('.part_pictxt_2 h3 a'), hot
     url = "http://www.ettoday.net" + titles.get('href')
 
     detailReq = requests.get(url)
-    detailSoup = BeautifulSoup(detailReq.text)
+    detailSoup = BeautifulSoup(detailReq.text, "html.parser")
 
     title = titles.text
 
@@ -29,9 +26,9 @@ for titles , content, time in zip(hotnewsSoup.select('.part_pictxt_2 h3 a'), hot
     url = detailSoup.select('head link[rel="canonical"]')[0].get('href')
     fbReq = requests.get('https://graph.facebook.com/?ids=' + url)
     fbData = fbReq.json()
-    shareCount = fbData[url]['shares'] if fbData[url].has_key('shares') else 0 
+    shareCount = fbData[url]['shares'] if fbData[url].has_key('shares') else 0
     likeCount = shareCount
-    commentCount = fbData[url]['comments'] if fbData[url].has_key('comments') else 0 
+    commentCount = fbData[url]['comments'] if fbData[url].has_key('comments') else 0
     content = content.text
 
     time = time.text
@@ -43,16 +40,14 @@ for titles , content, time in zip(hotnewsSoup.select('.part_pictxt_2 h3 a'), hot
 
     hotnews.append([title, category, keywords, shareCount, likeCount, commentCount, content, url, time, img])
 
+col_ettoday = MongoClient('localhost', 27017).iguana.ettoday
+col_ettoday_count = MongoClient('localhost', 27017).iguana.ettoday_count
 for lists in hotnews:
     [title, category, keywords, shareCount, likeCount, commentCount, content, url, time, img] = lists
-    dataObject = {
+    content_dataObject = {
         "title": title, # 標題
         "category": category, # 分類
         "keywords" : keywords, #關鍵字
-        "shareCount": shareCount, # 分享數
-        "likeCount": likeCount, # 按讚數
-        "commentCount": commentCount,
-        "browserCount": None, # 瀏覽數
         "content": content, #內文
         "comment" : [], # 評論
         "url" : url, # 網址
@@ -60,7 +55,30 @@ for lists in hotnews:
         "image" : img, # 圖片
         "source" : "Ettoday", # 來源
     }
-    collection.insert_one(dataObject)
+    col_ettoday.update_one(
+        {
+           "title": title
+        },
+        {"$set": content_dataObject},
+        upsert = True
+    )
+    news_id = col_ettoday.find_one({'title': title})['_id']
+    count_dataObject = {
+        "news_id" : news_id,
+        "shareCount": shareCount, # 分享數
+        "likeCount": likeCount, # 按讚數
+        "commentCount": commentCount, #評論數
+        "browserCount": None, # 瀏覽數
+    }
+    col_ettoday_count.update_one(
+        {
+        "shareCount": shareCount, # 分享數
+        "likeCount": likeCount, # 按讚數
+        "commentCount": commentCount #評論數
+        },
+        {"$set": count_dataObject} ,
+        upsert = True
+    )
 
 
 
